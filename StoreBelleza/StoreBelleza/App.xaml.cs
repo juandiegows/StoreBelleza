eb50654248e37317ed29953bf47e32e31cfaecf1
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-11-16 19:20:45 -0500

    Add project files.

---
+++ StoreBelleza/StoreBelleza/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using StoreBelleza.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StoreBelleza
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new MainPage());
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ StoreBelleza/StoreBelleza/View/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using StoreBelleza.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StoreBelleza
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new addProducts());
+        }
+    }
+}
