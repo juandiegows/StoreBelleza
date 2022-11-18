68b03b5b2e610d2cdbab5d1d371baa2332f5b40e
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-11-18 02:52:03 -0500

    manager products 100%

--- StoreBelleza/StoreBelleza/View/ManageProduct.xaml.cs
+++ StoreBelleza/StoreBelleza/View/ManageProduct.xaml.cs
@@ -1,4 +1,6 @@
-﻿using StoreBelleza.ViewModels;
+﻿using StoreBelleza.Controller;
+using StoreBelleza.Model;
+using StoreBelleza.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,42 @@ namespace StoreBelleza.View
         {
             InitializeComponent();
             BindingContext = model = new ProductModel(App.SQLiteHelper);
+        }
+
+        private async void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            await Delete(sender);
+        }
+
+        private async Task Delete(object sender)
+        {
+            if ((await DisplayAlert("Question", "are you sure, you want to delete?", "yes", "no")))
+            {
+                Product product = (sender as Xamarin.Forms.View).BindingContext as Product;
+                new ProductController(App.SQLiteHelper).Delete(product);
+                model.collectionProduct.Remove(product);
+                BindingContext = null;
+                BindingContext = model;
+            }
+        }
+
+        private async void btnSupply_Clicked(object sender, EventArgs e)
+        {
+            if ((await DisplayAlert("Question", "are you sure, you want to supply?", "yes", "no")))
+            {
+                string count = await DisplayPromptAsync("Question", "how many products you want to supply?", keyboard: Keyboard.Numeric);
+                if (count == null)
+                {
+                    await DisplayAlert("error", "the  field is required", "Ok");
+                    return;
+                }
+                int.TryParse(count, out int cant);
+                Product product = (sender as Xamarin.Forms.View).BindingContext as Product;
+                product.Count += cant;
+                new ProductController(App.SQLiteHelper).Update(product);
+                BindingContext = null;
+                BindingContext = model;
+            }
         }
     }
 }
